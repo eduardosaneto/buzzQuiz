26b0dbe0307afa208c83adcc6294ed12d1215f85
thiagoribb <thiribeiro142@gmail.com>
2021-04-27 16:20:15 -0300

    preparando review

--- assets/javascript/voltaTela.js
+++
@@ -1,9 +0,0 @@
-function voltaPaginaPrincipal() {
-
-    const paginaCriacaoQuiz = document.querySelector(".criacao-quiz");
-    paginaCriacaoQuiz.classList.add("escondido");   
-    const paginaPrincipal = document.querySelector(".pagina-principal");
-    paginaPrincipal.classList.remove("escondido");
-
-    obterQuizes();
-}
